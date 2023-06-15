9f53f75f9d3018dbed793fb699514216b48b8e9d
glebsolovyov <solovyovgleb1@gmail.com>
2023-06-15 05:07:04 +0300

    some updates

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -2,9 +2,8 @@ from aiogram import Dispatcher
 
 
 def register_all_handlers(dispatcher: Dispatcher) -> None:
-    from first_trimester.handlers import register_first_trimester_handlers
-    from second_trimester.handlers import register_second_trimester_handlers
-
+    from modules.first_trimester.handlers import register_first_trimester_handlers
+    from modules.second_trimester.handlers import register_second_trimester_handlers
     handlers = [
         register_first_trimester_handlers,
         register_second_trimester_handlers
