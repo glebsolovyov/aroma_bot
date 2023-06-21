1d647e66179ae11f48f39f8fe4ae119bdf48cfa6
glebsolovyov <solovyovgleb1@gmail.com>
2023-06-22 00:31:42 +0300

    Update bot for yandex cloud

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.11-alpine
-
-COPY requirements.txt /temp/requirements.txt
-COPY app /app
-
-WORKDIR /app
-
-RUN pip install -r /temp/requirements.txt
--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import json
+
 from aiogram import Bot, Dispatcher, types, executor
 
 import config
@@ -14,6 +16,7 @@ async def __on_start_up(dispathcer: Dispatcher) -> None:
     register_all_handlers(dispathcer)
 
 
+
 @dp.message_handler(commands=['start'])
 async def command_start(message: types.Message):
     await bot.send_message(message.chat.id, start_messages.MESSAGE_FOR_COMMAND_START)
@@ -21,5 +24,14 @@ async def command_start(message: types.Message):
                            reply_markup=command_start_keyboard)
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True, on_startup=__on_start_up)
+async def process_event(event, dp: Dispatcher):
+    update = json.loads(event['body'])
+    Bot.set_current(dp.bot)
+    update = types.Update.to_object(update)
+    await dp.process_update(update)
+
+
+async def handler(event, context):
+    await __on_start_up(dp)
+    await process_event(event, dp)
+    return {'statusCode': 200, 'body': 'ok'}
