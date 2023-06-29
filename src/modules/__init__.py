43d60140270f3cba0f8e6878765a2dcde87a9e22
glebsolovyov <solovyovgleb1@gmail.com>
2023-06-29 03:26:20 +0300

    add module taking care of the house

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -8,7 +8,9 @@ def register_all_handlers(dispatcher: Dispatcher) -> None:
     from .mom_of_baby.handlers import register_mom_of_baby_handlers
     from .assistance_to_family.handlers import register_assistance_to_family_handlers
     from .beauty.handlers import register_beauty_handlers
+    from .taking_care_of_the_house.handlers import register_taking_care_of_the_house_handlers
     from .tariff.handlers import register_all_tariff_handlers
+
     handlers = [
         register_first_trimester_handlers,
         register_second_trimester_handlers,
@@ -16,6 +18,7 @@ def register_all_handlers(dispatcher: Dispatcher) -> None:
         register_mom_of_baby_handlers,
         register_assistance_to_family_handlers,
         register_beauty_handlers,
+        register_taking_care_of_the_house_handlers,
         register_all_tariff_handlers,
     ]
 
